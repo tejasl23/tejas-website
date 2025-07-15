3745892f6bea20a7323ff6f2151dad1803e503bf
tejasl23 <tejaslkr23@gmail.com>
2025-07-14 21:32:50 -0400

    fix: weird git stuff

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  mode: 'development',
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true
+  }
+};
